28c3600984e987aaa171e0d36cf092bebcec46d0
Sandor Weisz <santheo@gmail.com>
2017-12-03 21:22:24 -0600

    Default values for context variables

--- globals.php
+++ globals.php
@@ -73,9 +73,9 @@ function render($template, $vars = array()) {
 		->findOne();
 
 	Global $twig;
-	$vars['user']       = $_SESSION['user'];
-	$vars['user_id']    = $_SESSION['user_id'];
-	$vars['alert']      = $_SESSION['alert_message'];
+	$vars['user']       = $_SESSION['user']??null;
+	$vars['user_id']    = $_SESSION['user_id']??0;
+	$vars['alert']      = $_SESSION['alert_message']??null;
 	$vars['time']       = strftime('%c');
 	$vars['latestNews'] = $latestNews;
 
