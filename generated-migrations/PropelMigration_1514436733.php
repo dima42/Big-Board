bd0eae4ea7ec099753da38a1d0616d2964f6eb16
Sandor Weisz <santheo@gmail.com>
2017-12-27 23:10:53 -0600

    Redo migration to hard code Cats and Skills

--- generated-migrations/PropelMigration_1514436733.php
+++ generated-migrations/PropelMigration_1514436733.php
@@ -4,43 +4,38 @@ use Propel\Generator\Manager\MigrationManager;
 
 /**
  * Data object containing the SQL and PHP code to migrate the database
- * up to version 1514429395.
- * Generated on 2017-12-28 02:49:55 by sandor
+ * up to version 1514436733.
+ * Generated on 2017-12-28 04:52:13 by sandor
  */
-class PropelMigration_1514429395
-{
-    public $comment = '';
 
-    public function preUp(MigrationManager $manager)
-    {
-        // add the pre-migration code here
-    }
+class PropelMigration_1514436733 {
+	public $comment = '';
 
-    public function postUp(MigrationManager $manager)
-    {
-        // add the post-migration code here
-    }
+	public function preUp(MigrationManager $manager) {
+		// add the pre-migration code here
+	}
 
-    public function preDown(MigrationManager $manager)
-    {
-        // add the pre-migration code here
-    }
+	public function postUp(MigrationManager $manager) {
+		// add the post-migration code here
+	}
 
-    public function postDown(MigrationManager $manager)
-    {
-        // add the post-migration code here
-    }
+	public function preDown(MigrationManager $manager) {
+		// add the pre-migration code here
+	}
 
-    /**
-     * Get the SQL statements for the Up migration
-     *
-     * @return array list of the SQL strings to execute for the Up migration
-     *               the keys being the datasources
-     */
-    public function getUpSQL()
-    {
-        return array (
-  'palindrome' => '
+	public function postDown(MigrationManager $manager) {
+		// add the post-migration code here
+	}
+
+	/**
+	 * Get the SQL statements for the Up migration
+	 *
+	 * @return array list of the SQL strings to execute for the Up migration
+	 *               the keys being the datasources
+	 */
+	public function getUpSQL() {
+		return array(
+			'palindrome' => '
 # This is a fix for InnoDB in MySQL >= 4.1.x
 # It "suspends judgement" for fkey relationships until are tables are set.
 SET FOREIGN_KEY_CHECKS = 0;
@@ -54,8 +49,14 @@ CREATE TABLE `topic`
     `tree_left` INTEGER,
     `tree_right` INTEGER,
     `tree_level` INTEGER,
+    `tree_scope` INTEGER,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
+
+INSERT INTO `topic` (`id`, `title`, `slack_channel`, `slack_channel_id`, `tree_left`, `tree_right`, `tree_level`, `tree_scope`)
+VALUES
+    (1, "Category", NULL, NULL, 1, 2, 0, 1),
+    (2, "Skill", NULL, NULL, 1, 2, 0, 2);
 
 CREATE TABLE `puzzleTopic`
 (
@@ -79,19 +80,18 @@ CREATE TABLE `puzzleTopic`
 # This restores the fkey checks, after having unset them earlier
 SET FOREIGN_KEY_CHECKS = 1;
 ',
-);
-    }
+		);
+	}
 
-    /**
-     * Get the SQL statements for the Down migration
-     *
-     * @return array list of the SQL strings to execute for the Down migration
-     *               the keys being the datasources
-     */
-    public function getDownSQL()
-    {
-        return array (
-  'palindrome' => '
+	/**
+	 * Get the SQL statements for the Down migration
+	 *
+	 * @return array list of the SQL strings to execute for the Down migration
+	 *               the keys being the datasources
+	 */
+	public function getDownSQL() {
+		return array(
+			'palindrome' => '
 # This is a fix for InnoDB in MySQL >= 4.1.x
 # It "suspends judgement" for fkey relationships until are tables are set.
 SET FOREIGN_KEY_CHECKS = 0;
@@ -103,7 +103,7 @@ DROP TABLE IF EXISTS `puzzleTopic`;
 # This restores the fkey checks, after having unset them earlier
 SET FOREIGN_KEY_CHECKS = 1;
 ',
-);
-    }
+		);
+	}
 
 }
