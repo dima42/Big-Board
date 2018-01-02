cd6a4a7894d8136dce517dc8661b88cf007ad18e
Sandor Weisz <santheo@gmail.com>
2018-01-02 12:59:39 -0600

    Avatars on /roster

--- controller.php
+++ controller.php
@@ -267,7 +267,7 @@ function allMembers($response) {
 		->leftJoin('PuzzleMember')
 		->withColumn('PuzzleMember.PuzzleId', 'PuzzleId')
 		->orderByFullName()
-		->select(array('Id', 'FullName', 'Strengths', 'PhoneNumber', 'SlackId', 'PuzzleId'))
+		->select(array('Id', 'FullName', 'Strengths', 'PhoneNumber', 'SlackId', 'PuzzleId', 'Avatar'))
 		->find()
 		->toArray();
 
