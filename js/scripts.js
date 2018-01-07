239ad4b9331e4739dde0cc4a3d7415522b6e7e57
Sandor Weisz <santheo@gmail.com>
2018-01-07 17:33:42 -0600

    Move Mustache templates inline.

--- js/scripts.js
+++ js/scripts.js
@@ -28,38 +28,37 @@ var loadPuzzleTagData = function(url, callback) {
 var loadPuzzleTemplate = function(response, callback, parentListName, parentIDName) {
     var data = response;
     var now = Date.now();
-    $.get('/templates/puzzle_row.mst', function(template) {
-        $.each(data, function(key, puzzleData) {
-            var ssID = puzzleData['SpreadsheetId'];
-            ssID = ssID.replace(/^.+ccc\?key=/, "");
-            puzzleData['SpreadsheetURL'] = "https://docs.google.com/spreadsheets/d/" + ssID;
-            puzzleData['SlackURL'] = "http://" + slackDomain + ".slack.com/messages/"+puzzleData['SlackChannel'];
-            puzzleData['SinceCreated'] = now - new Date(puzzleData['CreatedAt']);
-            puzzleData['SinceUpdated'] = now - new Date(puzzleData['UpdatedAt']);
+    var template = $('#puzzle_row').html();
+    $.each(data, function(key, puzzleData) {
+        var ssID = puzzleData['SpreadsheetId'];
+        ssID = ssID.replace(/^.+ccc\?key=/, "");
+        puzzleData['SpreadsheetURL'] = "https://docs.google.com/spreadsheets/d/" + ssID;
+        puzzleData['SlackURL'] = "http://" + slackDomain + ".slack.com/messages/"+puzzleData['SlackChannel'];
+        puzzleData['SinceCreated'] = now - new Date(puzzleData['CreatedAt']);
+        puzzleData['SinceUpdated'] = now - new Date(puzzleData['UpdatedAt']);
 
-            var rendered = Mustache.render(template, puzzleData);
+        var rendered = Mustache.render(template, puzzleData);
 
-            var parents = puzzleData[parentListName];
-            if (!parents || parents.length == 0) {
-                var emptySet = {};
-                emptySet[parentIDName] = 0;
-                parents = [emptySet];
-            }
-
-            $.each(parents, function(key, parent) {
-                var tableID = parent[parentIDName];
-                var tbody = $('#table-' + tableID).find('tbody');
-                if (tableID == puzzleData['Id']) {
-                    tbody.prepend(rendered);
-                } else {
-                    tbody.append(rendered);
-                }
-            });
-        });
-        if (callback) {
-            callback();
+        var parents = puzzleData[parentListName];
+        if (!parents || parents.length == 0) {
+            var emptySet = {};
+            emptySet[parentIDName] = 0;
+            parents = [emptySet];
         }
+
+        $.each(parents, function(key, parent) {
+            var tableID = parent[parentIDName];
+            var tbody = $('#table-' + tableID).find('tbody');
+            if (tableID == puzzleData['Id']) {
+                tbody.prepend(rendered);
+            } else {
+                tbody.append(rendered);
+            }
+        });
     });
+    if (callback) {
+        callback();
+    }
 };
 
 var loadMemberData = function(url, callback) {
@@ -69,24 +68,23 @@ var loadMemberData = function(url, callback) {
 };
 
 var loadMemberTemplate = function(response, callback) {
-    $.get('/templates/member_row.mst', function(template) {
-        $.each(response, function(key, memberData) {
-            if(memberData['PuzzleId'] != null) {
-                memberData['PuzzleTitle'] = $('#table-' + memberData['PuzzleId']).data('title');
-            }
-            memberData['slackDomain'] = slackDomain;
-
-            var rendered = Mustache.render(template, memberData);
-            var puzzleID = memberData['PuzzleId'] || 0;
-            var tbody = $('#table-' + puzzleID).find('tbody');
-            tbody.append(rendered);
-
-            $('#table-alpha tbody').append(rendered);
-        });
-        if (callback) {
-            callback();
+    var template = $('#member_row').html();
+    $.each(response, function(key, memberData) {
+        if(memberData['PuzzleId'] != null) {
+            memberData['PuzzleTitle'] = $('#table-' + memberData['PuzzleId']).data('title');
         }
+        memberData['slackDomain'] = slackDomain;
+
+        var rendered = Mustache.render(template, memberData);
+        var puzzleID = memberData['PuzzleId'] || 0;
+        var tbody = $('#table-' + puzzleID).find('tbody');
+        tbody.append(rendered);
+
+        $('#table-alpha tbody').append(rendered);
     });
+    if (callback) {
+        callback();
+    }
 };
 
 
