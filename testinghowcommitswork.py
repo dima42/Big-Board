14436dc99b76291d3a63a00787bc38b43af3517e
angesw <79068710+angesw@users.noreply.github.com>
2021-02-17 01:25:48 -0500

    is this what reverts it

--- testinghowcommitswork.py
+++ testinghowcommitswork.py
@@ -1,5 +1,9 @@
 Ange = sleepy
+<<<<<<< HEAD
 Angeface = not_going_to_bed
 Angepants = cold
 Angehead = a_little_hungry
 Angeasaurus_Rex = wants_to_be_left_alone
+=======
+Ange = not_going_to_bed
+>>>>>>> parent of ae1c614 (line3)
