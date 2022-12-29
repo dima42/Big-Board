ee3f26c4b3c9533c367f0808327d605324a65baf
Shawn Ligocki <sligocki@gmail.com>
2022-12-29 15:31:57 -0800

    serialize message to string.

--- slack.php
+++ slack.php
@@ -43,7 +43,7 @@ function createNewSlackChannel($slug) {
 			'name' => $slug
 		]);
 
-    error_log($slack_response);
+    error_log(serialize($slack_response));
 
     $id = $slack_response["channel"]["id"];
 
